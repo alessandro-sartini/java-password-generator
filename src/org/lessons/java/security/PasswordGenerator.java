c8ffceb73488b47af8090d7b7d0126d73e0aea6e
alessandro-sartini <ale.sartini.98@gmail.com>
2025-04-29 16:32:09 +0200

    first commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,45 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    // Salvare in opportune variabili il nome, cognome, colore preferito 
+    // e data di nascita di un utente suddivisa in giorno, mese e anno in numero   
+    public static void main(String[] args) {
+        String nome;
+        String cognome;
+        String colore;
+        
+        int giornoDiNascita;
+        int meseDiNascita;
+        int annoDiNascita;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Inserisci il tuo nome");
+
+        nome = sc.nextLine();
+
+        System.out.println("Inserisci il tuo cognome");
+
+        cognome = sc.nextLine();
+
+        System.out.println("Inserisci il tuo colore preferito");
+
+        colore = sc.nextLine();
+        System.out.println("Inserisci il giorno di nascita");
+
+        giornoDiNascita = sc.nextInt();
+
+        System.out.println("Inserisci il mese di nascita");
+
+        meseDiNascita = sc.nextInt();
+
+        System.out.println("Inserisci l'anno di nascita");
+
+        annoDiNascita = sc.nextInt();
+        int sommaPsw= annoDiNascita+meseDiNascita+giornoDiNascita;
+
+        System.out.println("Password genrata: "+ nome+"-"+cognome+"-"+colore+"-"+sommaPsw);
+    }
+}
